19d2e3c3107148f0a031cbae2e18d38185d4704c
tessav <tessavoon@gmail.com>
2015-12-14 22:29:31 +0800

    all

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+
+.controller('requirementsCtrl', function($scope) {
+    $scope.customSpice = "wasabi";
+    $scope.spice = 'very';
+
+    $scope.spicy = function(spice) {
+      $scope.spice = spice;
+    };
+})
+
+.controller('modulesCtrl', function($scope) {
+
+})
+
+.controller('profileCtrl', function($scope) {
+
+})
---
+++ www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('tabsController.requirements', {
+      url: '/page2',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/requirements.html',
+          controller: 'requirementsCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.modules', {
+      url: '/page3',
+      views: {
+        'tab2': {
+          templateUrl: 'templates/modules.html',
+          controller: 'modulesCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.profile', {
+      url: '/page4',
+      views: {
+        'tab3': {
+          templateUrl: 'templates/profile.html',
+          controller: 'profileCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('tabsController', {
+      url: '/page1',
+      abstract:true,
+      templateUrl: 'templates/tabsController.html'
+    })
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page1/page2');
+
+});
